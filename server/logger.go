638904ce8b17fbc84eca1168ab7be5b281a276fa
Javed Khan <tuxcanfly@gmail.com>
2020-08-28 23:07:34 +0530

    server: fix logger

--- server/logger.go
+++ server/logger.go
@@ -34,27 +34,27 @@ func logger(prefix, server *string) {
 			break
 		}
 		if err != nil {
-			log.Fatalf("failed to connect: %s", err)
+			log.Printf("failed to receive instance: %s", err)
 		}
 		log.Println(instance)
 
 		app, err := client.GetApplication(context.Background(), &pb.ApplicationId{Id: instance.Appid})
 		if err != nil {
-			log.Fatalf("failed to app: %s", err)
+			log.Printf("failed to get app: %s", err)
 		}
 
 		log.Println(app)
 
 		device, err := client.GetDevice(context.Background(), &pb.DeviceId{Id: instance.Deviceid})
 		if err != nil {
-			log.Fatalf("failed to device: %s", err)
+			log.Printf("failed to get device: %s", err)
 		}
 
 		log.Println(device)
 
 		receiverid, err := client.RegisterReceive(context.Background(), &pb.InstanceId{Id: instance.Id})
 		if err != nil {
-			log.Fatalf("failed to register: %s", err)
+			log.Printf("failed to register receive: %s", err)
 		}
 
 		log.Println(receiverid)
@@ -62,7 +62,7 @@ func logger(prefix, server *string) {
 		logfilepath := path.Join(*prefix, app.Id, device.Id, instance.Id)
 		err = os.MkdirAll(logfilepath, 0777)
 		if err != nil {
-			log.Fatalf("failed to mkdir: %s", err)
+			log.Printf("failed to mkdir: %s", err)
 		}
 
 		log.Println(logfilepath)
@@ -70,14 +70,14 @@ func logger(prefix, server *string) {
 		go func(instance *pb.Instance, app *pb.Application, device *pb.Device, receiverid *pb.ReceiverId, logfilepath string) {
 			stream, err := client.Receive(context.Background(), receiverid)
 			if err != nil {
-				log.Fatalf("failed to receive: %s", err)
+				log.Printf("failed to receive: %s", err)
 			}
 
 			log.Printf("Started logger for instance: %s\n", instance.Id)
 
 			logfile, err := os.OpenFile(path.Join(logfilepath, "logs.txt"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 			if err != nil {
-				log.Fatalf("failed to open file: %s", err)
+				log.Printf("failed to open file: %s", err)
 			}
 			defer logfile.Close()
 
@@ -87,7 +87,7 @@ func logger(prefix, server *string) {
 					break
 				}
 				if err != nil {
-					log.Fatalf("failed to connect: %s", err)
+					log.Printf("failed to connect: %s", err)
 				}
 				logline := fmt.Sprintf("%v: level = %v, app = %v; device = %v; msg = %v\n",
 					in.Timestamp, in.Level, app, device, in.Msg)
