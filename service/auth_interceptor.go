7153ac61cbea5f0e8c5669ca59b8e45d096d3b32
abdul <abdul@loggy.sh>
2021-07-08 02:04:30 +0100

    add auth interceptor

---
+++ service/auth_interceptor.go
@@ -0,0 +1,75 @@
+package service
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+	"log"
+)
+
+type AuthInterceptor struct {
+	jwtManager      *JWTManager
+}
+
+func NewAuthInterceptor(jwtManager *JWTManager) *AuthInterceptor {
+	return &AuthInterceptor{jwtManager}
+}
+
+func (interceptor *AuthInterceptor) Unary() grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context,
+		req interface{},
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (interface{}, error) {
+		log.Println("--> unary interceptor: ", info.FullMethod)
+
+		err := interceptor.authorize(ctx, info.FullMethod)
+		if err != nil {
+			return nil, err
+		}
+
+		return handler(ctx, req)
+	}
+}
+
+func (interceptor *AuthInterceptor) Stream() grpc.StreamServerInterceptor {
+	return func(
+		srv interface{},
+		stream grpc.ServerStream,
+		info *grpc.StreamServerInfo,
+		handler grpc.StreamHandler,
+	) error {
+		log.Println("--> stream interceptor: ", info.FullMethod)
+
+		err := interceptor.authorize(stream.Context(), info.FullMethod)
+		if err != nil {
+			return err
+		}
+
+		return handler(srv, stream)
+	}
+
+}
+
+func (interceptor *AuthInterceptor) authorize(ctx context.Context, method string) error {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return status.Errorf(codes.Unauthenticated, "metadata is not provided")
+	}
+
+	values := md["authorization"]
+	if len(values) == 0 {
+		return status.Errorf(codes.Unauthenticated, "authorization token is not provided")
+	}
+
+	accessToken := values[0]
+	_, err := interceptor.jwtManager.Verify(accessToken)
+	if err != nil {
+		return status.Errorf(codes.Unauthenticated, "access token is invalid: %v", err)
+	}
+
+	return status.Error(codes.PermissionDenied, "no permission to access this RPC")
+}
