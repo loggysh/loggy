4a5bddb249d8c12cc0794e8511a0adb3f135fa09
imran <imohammed23@gmail.com>
2021-08-18 18:17:58 +0530

    update proto package installs

--- pre-make.sh
+++ pre-make.sh
@@ -7,16 +7,16 @@ echo $UNAME
 if [ "$UNAME" == "Linux" ] ; then
 	sudo apt-get install make &&
 	sudo apt-get install -y protobuf-compiler &&
-	sudo apt  install golang-goprotobuf-dev
+	sudo apt-get install golang-goprotobuf-dev
 elif [ "$UNAME" == "Darwin" ] ; then
 	brew install make
 	brew install protobuf
 fi
 
-
-go get -u google.golang.org/grpc &&
-go get -u github.com/golang/protobuf/{proto,protoc-gen-go} &&
-go get -u google.golang.org/grpc/cmd/protoc-gen-go-grpc &&
-go get -u github.com/gin-gonic/gin &&
-go get -u golang.org/x/crypto/bcrypt &&
+go get -u google.golang.org/grpc &
+go get -u google.golang.org/protobuf/cmd/protoc-gen-go &
+go get -u google.golang.org/grpc/cmd/protoc-gen-go-grpc &
+go get -u github.com/golang/protobuf/proto &
+go get -u github.com/gin-gonic/gin &
+go get -u golang.org/x/crypto/bcrypt &
 go get -u github.com/golang-jwt/jwt@v3.2.0
