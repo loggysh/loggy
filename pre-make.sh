881cf98ca03a9b021fef09148a73de870d029952
imran <imohammed23@gmail.com>
2021-08-29 13:49:31 +0530

    upgrade to jwt v4

--- pre-make.sh
+++ pre-make.sh
@@ -16,4 +16,4 @@ go get -u google.golang.org/grpc/cmd/protoc-gen-go-grpc && \
 go get -u github.com/golang/protobuf/proto && \
 go get -u github.com/gin-gonic/gin && \
 go get -u golang.org/x/crypto/bcrypt && \
-go get -u github.com/golang-jwt/jwt@v3.2.0
+go get -u github.com/golang-jwt/jwt/v4
